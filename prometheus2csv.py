7d767a0129a6d87636d5a10fca2587f9b11df447
Alexey Aksenov <ezh@ezh.msk.ru>
2019-05-11 15:25:41 +0300

    Drop CSV part, prepare for exporter

--- prometheus2csv.py
+++ prometheus2csv.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # Filename: metrics2csv.py
 
-import csv
 import requests
 import sys
 import getopt
@@ -10,14 +9,13 @@ import time
 import logging
 
 PROMETHEUS_URL = ''
-CONTAINER = ''
 QUERY_API = '/api/v1/query'
 RANGE_QUERY_API = '/api/v1/query_range'
 RESOLUTION = '' # default: 10s
-OUTPUTFILE = '' # default: result.csv
 START = '' # rfc3339 | unix_timestamp
 END = '' # rfc3339 | unix_timestamp
-PERIOD = 60 # unit: minute, default 60
+PERIOD = '' # unit: second
+SELECTOR = '__name__=~".+"'
 
 def main():
     handle_args(sys.argv[1:])
@@ -25,22 +23,18 @@ def main():
     metricnames = query_metric_names()
     logging.info("Querying metric names succeeded, metric number: %s", len(metricnames))
 
-    csvset = query_metric_values(metricnames=metricnames)
-    logging.info("Querying metric values succeeded, rows of data: %s", len(csvset))
-
-    write2csv(filename=OUTPUTFILE, metricnames=metricnames, dataset=csvset)
+    forward_metric_values(metricnames=metricnames)
 
 def handle_args(argv):
     global PROMETHEUS_URL
-    global OUTPUTFILE
-    global CONTAINER
     global RESOLUTION
     global START
     global END
     global PERIOD
+    global SELECTOR
 
     try:
-        opts, args = getopt.getopt(argv, "h:o:c:s:", ["host=", "outfile=", "container=", "step=", "help", "start=", "end=", "period="])
+        opts, args = getopt.getopt(argv, "h:c:s:p:", ["host=", "container=", "step=", "period=", "help", "start=", "end="])
     except getopt.GetoptError as error:
         logging.error(error)
         print_help_info()
@@ -52,10 +46,6 @@ def handle_args(argv):
             sys.exit()
         elif opt in ("-h", "--host"):
             PROMETHEUS_URL = arg
-        elif opt in ("-o", "--outfile"):
-            OUTPUTFILE = arg
-        elif opt in ("-c", "--container"):
-            CONTAINER = arg
         elif opt in ("-s", "--step"):
             RESOLUTION = arg
         elif opt == "--start":
@@ -69,38 +59,38 @@ def handle_args(argv):
         logging.error("You should use -h or --host to specify your prometheus server's url, e.g. http://prometheus:9090")
         print_help_info()
         sys.exit(2)
-    elif CONTAINER == '':
-        logging.error("You should use -c or --container to specify the name of the container which you want to query all the metrics of")
-        print_help_info()
-        sys.exit(2)
 
-    if OUTPUTFILE == '':
-        OUTPUTFILE = 'result.csv'
-        logging.warning("You didn't specify output file's name, will use default name %s", OUTPUTFILE)
     if RESOLUTION == '':
         RESOLUTION = '10s'
         logging.warning("You didn't specify query resolution step width, will use default value %s", RESOLUTION)
     if PERIOD == '' and START == '' and END == '':
         PERIOD = 10
-        logging.warning("You didn't specify query period or start&end time, will query the latest %s miniutes' data as a test", PERIOD)
+        logging.warning("You didn't specify query period or start&end time, will query the latest %s seconds' data as a test", PERIOD)
+    if args:
+        SELECTOR = ",".join(args)
+
+    import ptpdb
+    ptpdb.set_trace()
 
 def print_help_info():
     print('')
-    print('Metrics2CSV Help Info')
-    print('    metrics2csv.py -h <prometheus_url> -c <container_name> [-o <outputfile>]')
-    print('or: metrics2csv.py --host=<prometheus_url> --container=<container_name> [--outfile=<outputfile>]')
+    print('Metrics2StatsD Help Info')
+    print('    metrics2statsd.py -h <prometheus_url> label_selector')
+    print('or: metrics2statsd.py --host=<prometheus_url> label_selector')
     print('---')
-    print('Additional options: --start=<start_timestamp_or_rfc3339> --end=<end_timestamp_or_rfc3339> --period=<get_for_most_recent_period(int miniutes)>')
+    print('Additional options: --start=<start_timestamp_or_rfc3339> --end=<end_timestamp_or_rfc3339> --period=<get_for_most_recent_period(int seconds)>')
     print('                    use start&end or only use period')
 
 def query_metric_names():
-    response = requests.get(PROMETHEUS_URL + QUERY_API, params={'query': 'sum by(__name__)({{name="{0}"}})'.format(CONTAINER)})
+    print('sum by(__name__)({{{}}})'.format(SELECTOR))
+    response = requests.get(PROMETHEUS_URL + QUERY_API, params={'query': 'sum by(__name__)({{{}}})'.format(SELECTOR)})
+    logging.info("Request {}".format(response.request.url))
     status = response.json()['status']
 
     if status == "error":
         logging.error(response.json())
         sys.exit(2)
-    
+
     results = response.json()['data']['result']
     metricnames = list()
     for result in results:
@@ -110,49 +100,20 @@ def query_metric_names():
     return metricnames
 
 
-def query_metric_values(metricnames):
-    csvset = dict()
-
+def forward_metric_values(metricnames):
     if PERIOD != '':
         end_time = int(time.time())
-        start_time = end_time - 60 * PERIOD
+        start_time = end_time - PERIOD
     else:
         end_time = END
         start_time = START
 
-    metric = metricnames[0]
-    response = requests.get(PROMETHEUS_URL + RANGE_QUERY_API, params={'query': '{0}{{name="{1}"}}'.format(metric,CONTAINER), 'start': start_time, 'end': end_time, 'step': RESOLUTION})
-    status = response.json()['status']
-
-    if status == "error":
-        logging.error(response.json())
-        sys.exit(2)
-
-    results = response.json()['data']['result']
-
-    if len(results) == 0:
-        logging.error(response.json())
-        sys.exit(2)
-
-    for value in results[0]['values']:
-        csvset[value[0]] = [value[1]]
-
-    for metric in metricnames[1:]:
-        response = requests.get(PROMETHEUS_URL + RANGE_QUERY_API, params={'query': '{0}{{name="{1}"}}'.format(metric,CONTAINER), 'start': start_time, 'end': end_time, 'step': RESOLUTION})
+    for metric in metricnames:
+        response = requests.get(PROMETHEUS_URL + RANGE_QUERY_API, params={'query': '{0}'.format(metric), 'start': start_time, 'end': end_time, 'step': RESOLUTION})
+        logging.info(response.request.url)
         results = response.json()['data']['result']
-        for value in results[0]['values']:
-            csvset[value[0]].append(value[1])
-
-    return csvset
-
-def write2csv(filename, metricnames, dataset):
-    with open(filename, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(['timestamp'] + metricnames)
-        for timestamp in sorted(dataset.keys(), reverse=True):
-            writer.writerow([timestamp] + dataset[timestamp])
-        # for line in dataset:
-        #     writer.writerow([line] + dataset[line])
+        for element in results:
+            print(element)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
